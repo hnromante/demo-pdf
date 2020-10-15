14929aac30efaabd6e6e22356d627cda9b9712f8
HugoNR <hugonromante@outlook.com>
2020-10-15 16:27:20 -0300

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+      <p>Prueba de concepto</p>
+      </div>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+    <v-main>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  })
+});
+</script>
